f6db8da2a65aaa2a47f57835e27e7fcc6915444a
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-12-04 22:13:33 -0500

    updates to the backend and create api endpoints

---
+++ routes/api/nonProfit.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const nonProfitController = require("../../controllers/nonProfitController")
+
+router.route("/")
+    .get(nonProfitController.findAll)
+    .post(nonProfitController.save);
+
+router.route("/:id")
+    .get(nonProfitController.findOne)
+    .delete(nonProfitController.remove);
+
+module.exports = router;
