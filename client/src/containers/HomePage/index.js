fbc0fbf7374abb91bb95a813c729e129984fa222
Casey Maltese <caseymaltese@gmail.com>
2020-12-09 22:16:31 -0500

    update className added Logout to NavTab updated text format

--- client/src/containers/HomePage/index.js
+++ client/src/containers/HomePage/index.js
@@ -9,15 +9,15 @@ const HomePage = () => {
 
         <Header />
 
-        <div class="row">
-            <div class="col-7">
+        <div className="row">
+            <div className="col-7">
                 <h2 id="hometitle">Welcome to Donate Miami!</h2>
             </div>
             <div clss="col-5"></div>
         </div>
 
-        <div class="row">
-            <div class="col-9">
+        <div className="row">
+            <div className="col-9">
                 <p id="homepage">
                     <Link to="/login">Login</Link> or
 
@@ -25,14 +25,14 @@ const HomePage = () => {
 
                     to make a donation to a local charity in Miami-Dade County.
                 </p>
-
-                <Link to="/dashboard">Go to Dashboard</Link>
+{/* 
+                <Link to="/dashboard">Go to Dashboard</Link> */}
             </div>
-            <div class="col-3"></div>
+            <div className="col-3"></div>
 
         </div>
 
-        <Footer />
+        {/* <Footer /> */}
     </div>
 }
 export default HomePage;
